07af98456c5fec4aaaaf480088924ebc813b255d
Hikaru02 <oao.hikaru.oao@gmail.com>
2013-11-03 05:58:03 +0900

    performance improvement

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict'
 //var log = console.log.bind(console)
 
-
-var exports = module.exports
+var HAS_NODEJS_API = true
+var HAS_BOM_DOM_API = false
 
 var SJIS_UNI_TABLES = require('./table/sjis-uni.js')
 var SJIS_UNI_TABLE_1 = SJIS_UNI_TABLES[0]
@@ -12,100 +12,218 @@ var SJIS_UNI_TABLE_SP = SJIS_UNI_TABLES[3]
 
 
 
-exports.fromSJIS = function (sjis_buf) {
 
-	var length = sjis_buf.length
-	sjis_buf = Buffer.concat([sjis_buf, new Buffer(1)])
+
+ArrayBuffer.isView = ArrayBuffer.isView || function (x) { // typeof x shuld be 'object'
+	return x.constructor.hasOwnProperty('BYTES_PER_ELEMENT') //簡易実装
+}
+
+if (HAS_NODEJS_API) {
+	Buffer.prototype.toArrayBuffer = Buffer.prototype.toArrayBuffer || function () {
+		return new Uint8Array(this).buffer
+	}
+}
+
+
+
+
+
+function conv(x, func) {
+
+	if (typeof x === 'object') {
+		if (x instanceof ArrayBuffer) {
+			return func(x)
+
+		} else if (HAS_NODEJS_API && Buffer.isBuffer(x)) {
+			return new Buffer(new Uint8Array(func(x.toArrayBuffer())))
+
+		} else if (ArrayBuffer.isView(x)) {
+			func(x.buffer)
+			return x
+		}
+
+		throw 'Unexpected Object Kind'
+
+	} else if (typeof x === 'string') {
+		throw 'Not Suported Yet'
+
+	} else throw 'Unexpected Type'
+}
+
+
+
+
+
+
+function SJIStoUTF8(sjis_buf) {
+
+	var sjis_len = sjis_buf.byteLength
+	//sjis_buf = Buffer.concat([sjis_buf, new Buffer(1)])
 
 	var uni_code = 0
-	var utf8_buf = new Buffer(0)
+	var utf8_buf = new ArrayBuffer(sjis_len*3) // 要検討（2~6）
 
-	function concat(utf8_codes) {
-		utf8_buf = Buffer.concat([utf8_buf, new Buffer(utf8_codes)])
-	}
+	var sjisView = new Uint8Array(sjis_buf)
+	var utf8View = new Uint8Array(utf8_buf)
 
-	var offset = 0
-	while (offset < length) {
+
+	var sjis_i = 0, utf8_i = 0
+
+	while (sjis_i < sjis_len-1) { //　最後の１つ前のオクテットまでの処理
 	
 		// sjis -> uni
-		var sjis_code = sjis_buf.readUInt8(offset, true)
+		var sjis_code = sjisView[sjis_i]
 
 		if (sjis_code === 0x7E)
 			uni_code = 0x203E
 		else if (sjis_code < 0x80)
 			uni_code = sjis_code
 		else if (sjis_code < 0xA0)
-			uni_code = SJIS_UNI_TABLE_1[sjis_buf.readUInt16BE(offset++, true) - 0x8140]
+			uni_code = SJIS_UNI_TABLE_1[(sjisView[sjis_i]<<8|sjisView[++sjis_i]) - 0x8140]
 		else if (sjis_code < 0xE0)
 			uni_code = SJIS_UNI_TABLE_2[sjis_code - 0xA0]
 		else
-			uni_code = SJIS_UNI_TABLE_3[sjis_buf.readUInt16BE(offset++, true) - 0xE040]
+			uni_code = SJIS_UNI_TABLE_3[(sjisView[sjis_i]<<8|sjisView[++sjis_i]) - 0xE040]
+
+		++sjis_i
 
 		// uni -> utf8
-		if (uni_code < 0x80)
-			concat([uni_code])
-		else if (uni_code < 0x800)
-			concat([uni_code>>>6|0xC0, uni_code&0x3F|0x80])
-		else if (uni_code < 0x10000)
-			concat([uni_code>>>12|0xE0, uni_code>>>6&0x3F|0x80, uni_code&0x3F|0x80])
-		else if (uni_code < 0x200000)
-			concat([uni_code>>>18|0xF0, uni_code>>>12&0x3F|0x80, uni_code>>>6&0x3F|0x80, uni_code&0x3F|0x80])
-		else if (uni_code < 0x4000000)
-			concat([uni_code>>>24|0xF8, uni_code>>>18&0x3F|0x80, uni_code>>>12&0x3F|0x80, uni_code>>>6&0x3F|0x80, uni_code&0x3F|0x80])
-		else
-			concat([uni_code>>>30|0xFC, uni_code>>>24&0x3F|0x80, uni_code>>>18&0x3F|0x80, uni_code>>>12&0x3F|0x80, uni_code>>>6&0x3F|0x80, uni_code&0x3F|0x80])
+		if (uni_code < 0x80) {
+			utf8View[utf8_i++] = uni_code
+		} else if (uni_code < 0x800) {
+			utf8View[utf8_i++] = uni_code>>>6|0xC0
+			utf8View[utf8_i++] = uni_code&0x3F|0x80
+		} else if (uni_code < 0x10000) {
+			utf8View[utf8_i++] = uni_code>>>12|0xE0
+			utf8View[utf8_i++] = uni_code>>>6&0x3F|0x80
+			utf8View[utf8_i++] = uni_code&0x3F|0x80
+		} else if (uni_code < 0x200000) {
+			utf8View[utf8_i++] = uni_code>>>18|0xF0
+			utf8View[utf8_i++] = uni_code>>>12&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>6&0x3F|0x80
+			utf8View[utf8_i++] = uni_code&0x3F|0x80
+		} else if (uni_code < 0x4000000) {
+			utf8View[utf8_i++] = uni_code>>>24|0xF8
+			utf8View[utf8_i++] = uni_code>>>18&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>12&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>6&0x3F|0x80
+			utf8View[utf8_i++] = uni_code&0x3F|0x80
+		} else {
+			utf8View[utf8_i++] = uni_code>>>30|0xFC
+			utf8View[utf8_i++] = uni_code>>>24&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>18&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>12&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>6&0x3F|0x80
+			utf8View[utf8_i++] = uni_code&0x3F|0x80
+		}
 
-		++offset
 	}
 
-	return utf8_buf
+	// 最後のオクテットが余った場合の処理
+	L: if (sjis_i === sjis_len-1) {
+		// sjis -> uni
+		var sjis_code = sjisView[sjis_i]
+
+		if (sjis_code === 0x7E)
+			uni_code = 0x203E
+		else if (sjis_code < 0x80)
+			uni_code = sjis_code
+		else if (sjis_code < 0xA0)　//異常なコード
+			break L
+		else if (sjis_code < 0xE0)
+			uni_code = SJIS_UNI_TABLE_2[sjis_code - 0xA0]
+		else　//異常なコード
+			break L
+
+		//++sjis_i
+
+		// uni -> utf8
+		if (uni_code < 0x80) {
+			utf8View[utf8_i++] = uni_code
+		} else if (uni_code < 0x800) {
+			utf8View[utf8_i++] = uni_code>>>6|0xC0
+			utf8View[utf8_i++] = uni_code&0x3F|0x80
+		} else if (uni_code < 0x10000) {
+			utf8View[utf8_i++] = uni_code>>>12|0xE0
+			utf8View[utf8_i++] = uni_code>>>6&0x3F|0x80
+			utf8View[utf8_i++] = uni_code&0x3F|0x80
+		} else if (uni_code < 0x200000) {
+			utf8View[utf8_i++] = uni_code>>>18|0xF0
+			utf8View[utf8_i++] = uni_code>>>12&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>6&0x3F|0x80
+			utf8View[utf8_i++] = uni_code&0x3F|0x80
+		} else if (uni_code < 0x4000000) {
+			utf8View[utf8_i++] = uni_code>>>24|0xF8
+			utf8View[utf8_i++] = uni_code>>>18&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>12&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>6&0x3F|0x80
+			utf8View[utf8_i++] = uni_code&0x3F|0x80
+		} else {
+			utf8View[utf8_i++] = uni_code>>>30|0xFC
+			utf8View[utf8_i++] = uni_code>>>24&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>18&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>12&0x3F|0x80
+			utf8View[utf8_i++] = uni_code>>>6&0x3F|0x80
+			utf8View[utf8_i++] = uni_code&0x3F|0x80
+		}
+	}
+
+	return utf8_buf.slice(0, utf8_i)
 
 }
 
 
 
-exports.toSJIS = function (utf8_buf) {
 
-	var length = utf8_buf.length
-	utf8_buf = Buffer.concat([utf8_buf, new Buffer(5)])
 
-	var uni_code = 0
-	var sjis_code = 0
-	var sjis_buf = new Buffer(0)
+
+function UTF8toSJIS(utf8_buf) {
+
+	var utf8_len = utf8_buf.byteLength
+
+	var uni_code = 0, sjis_code = 0
 	var index = 0
 
-	function concat(sjis_codes) {
-		sjis_buf = Buffer.concat([sjis_buf, new Buffer(sjis_codes)])
-	}
+	var sjis_buf = new ArrayBuffer(utf8_len*3) // 要検討（2~6）
+	
+	var utf8View = new Uint8Array(utf8_buf)
+	var sjisView = new Uint8Array(sjis_buf)
 
+
+	//function indexOf(table, code) { for (var i = 0; i < table.length; ++i) if (table[i] === code) return i; return -1 }
+
+	/*
 	function getBlockCode(n) {
 		var code = 0
 		for (var i = n-1; i >= 0; --i) {
-			code |= (utf8_buf.readUInt8(++offset, true)&0x3F) << i*6
+			code |= (utf8View[++utf8_i]&0x3F) << i*6
 		}
 		return code
 	}
+	*/
 
-	var offset = 0
-	while (offset < length) {
+	var utf8_i = 0, sjis_i = 0
+
+	while (utf8_i < utf8_len) {
 	
 		// utf8 -> uni
-		var utf8_code = utf8_buf.readUInt8(offset, true)
+		var utf8_code = utf8View[utf8_i]
 
 		if ((utf8_code&0x80) === 0)
 			uni_code = utf8_code
 		else if((utf8_code&0xE0) === 0xC0)
-			uni_code = (utf8_code&0x1F) << 6 | getBlockCode(1)
+			uni_code = (utf8_code&0x1F)<<6 | (utf8View[++utf8_i]&0x3F)
 		else if((utf8_code&0xF0) === 0xE0)
-			uni_code = (utf8_code&0xF) << 12 | getBlockCode(2)
+			uni_code = (utf8_code&0xF)<<12 | (utf8View[++utf8_i]&0x3F)<<6 | (utf8View[++utf8_i]&0x3F)
 		else if((utf8_code&0xF8) === 0xF0)
-			uni_code = (utf8_code&0x7) << 18 | getBlockCode(3)
+			uni_code = (utf8_code&0x7)<<18 | (utf8View[++utf8_i]&0x3F)<<12 | (utf8View[++utf8_i]&0x3F)<<6 | (utf8View[++utf8_i]&0x3F)
 		else if((utf8_code&0xFC) === 0xF8)
-			uni_code = (utf8_code&0x3) << 24 | getBlockCode(4)
+			uni_code = (utf8_code&0x3)<<24 | (utf8View[++utf8_i]&0x3F)<<18 | (utf8View[++utf8_i]&0x3F)<<12 | (utf8View[++utf8_i]&0x3F)<<6 | (utf8View[++utf8_i]&0x3F)
 		else if((utf8_code&0xFE) === 0xFC)
-			uni_code = (utf8_code&0x1) << 30 | getBlockCode(5)
+			uni_code = (utf8_code&0x1)<<30 | (utf8View[++utf8_i]&0x3F)<<24 | (utf8View[++utf8_i]&0x3F)<<18 | (utf8View[++utf8_i]&0x3F)<<12 | (utf8View[++utf8_i]&0x3F)<<6 | (utf8View[++utf8_i]&0x3F)
+		else continue
 
-		++offset
+		++utf8_i
 
 		// uni -> sjis
 		if (uni_code === 0x203E) 
@@ -123,19 +241,46 @@ exports.toSJIS = function (utf8_buf) {
 		else continue
 
 
-		if (sjis_code < 0x80)
-			concat([sjis_code])
-		else if (sjis_code < 0xA0)
-			concat([sjis_code>>>8, sjis_code&0xFF])
-		else if (sjis_code < 0xE0)
-			concat([sjis_code])
-		else
-			concat([sjis_code>>>8, sjis_code&0xFF])
+		if (sjis_code < 0x80) {
+			sjisView[sjis_i++] = sjis_code
+		} else if (sjis_code < 0xA0) {
+			sjisView[sjis_i++] = sjis_code>>>8
+			sjisView[sjis_i++] = sjis_code&0xFF
+		} else if (sjis_code < 0xE0) {
+			sjisView[sjis_i++] = sjis_code
+		} else {
+			sjisView[sjis_i++] = sjis_code>>>8
+			sjisView[sjis_i++] = sjis_code&0xFF
+		}
 
 	}
 
-	return sjis_buf
+	return sjis_buf.slice(0, sjis_i)
 
 }
 
 
+
+
+function KutenToSJIS(ku, ten) {
+
+
+}
+
+
+
+
+function fromSJIS(x) {
+	return conv(x, SJIStoUTF8)
+}
+
+function toSJIS(x) {
+	return conv(x, UTF8toSJIS)
+}
+
+
+
+module.exports = {
+	fromSJIS: fromSJIS,
+	toSJIS: toSJIS,
+}
